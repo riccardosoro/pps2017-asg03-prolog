27e60e5c850f1d81ad8d3dbd520991bc2c50cf28
riccardo soro <riccardosoro95@gmail.com>
2019-01-15 16:34:10 +0100

    size size2 sum

--- es_primo_blocco.pl
+++ es_primo_blocco.pl
@@ -15,3 +15,17 @@ search_two(X,[_|Xs]):-search_two(X,Xs).
 % looks for any Elem that occurs two times
 search_anytwo(X,[X|Xs]):-search(X,Xs).
 search_anytwo(X,[_|Xs]):-search_anytwo(X,Xs).
+
+% size(List,Size)
+% Size will contain the number of elements in List
+size([],0).
+size([_|T],M) :- size(T,N), M is N+1.
+
+% size2(List,Size)
+% Size2 will contain the number of elements in List,written using notation zero, s(zero), s(s(zero))..
+size2([],zero).
+size2([_|T],M) :- size2(T,N), M = s(N).
+
+% sum(List,Sum)
+sum([],0).
+sum([H|T],M) :- sum(T,N), M is N+H.
