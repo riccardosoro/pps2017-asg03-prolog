a367806e0519ec78eef5d8c3eef889b66443d70c
riccardo soro <riccardosoro95@gmail.com>
2019-01-22 17:50:31 +0100

    same allBigger sublist

--- es_primo_blocco.pl
+++ es_primo_blocco.pl
@@ -53,3 +53,29 @@ max(Xs,C2,A).
 max([X|Xs],C,A) :-
 C>=X,C2 is C,
 max(Xs,C2,A).
+
+
+% ES 3.1
+% same(List1,List2)
+% are the two lists the same?
+same([],[]).
+same([X|Xs],[X|Ys]):- same(Xs,Ys).
+
+% ES 3.2
+% all_bigger(List1,List2)
+% all elements in List1 are bigger than those in List2, 1 by 1
+% example: all_bigger([10,20,30,40],[9,19,29,39]).
+all_bigger([],[]).
+all_bigger([X|Xs],[Y|Ys]) :-
+X>Y,
+all_bigger(Xs,Ys).
+
+% ES 3.3
+% sublist(List1,List2)
+% List1 should be a subset of List2
+% example: sublist([1,2],[5,3,2,1]).
+sublist([X|Xs],List):-
+search(X,List),
+sublist(Xs,List).
+sublist([],List).
+
